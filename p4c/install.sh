2aa4e3966f0f5a6f67fbfe8d6c29de12768b362f
Praveen Tammana <praveen.thammana@gmail.com>
2018-01-19 10:00:44 -0500

    fixed p4c patch

--- p4c/install.sh
+++ p4c/install.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/p4lang/p4c.git
+git clone --recursive https://github.com/p4lang/p4c.git
 cd p4c
 git checkout da8ca81bd3b6d96195edb0cf9b8a7cf023fca3c8
 git apply ../p4c.patch
